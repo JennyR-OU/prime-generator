955efbc78c10615b3102b13b1fa8d8aa8bb87a8e
JennyR-OU <jennyanneriley@live.co.uk>
2025-01-18 12:03:05 +0000

    Changed prime again

--- src/main/java/uk/ac/open/primegenerator/Main.java
+++ src/main/java/uk/ac/open/primegenerator/Main.java
@@ -3,7 +3,7 @@ package uk.ac.open.primegenerator;
 public class Main {
 
     public int generatePrime() {
-        return 1;
+        return 7;
     }
 
     public static void main(String[] args) {
