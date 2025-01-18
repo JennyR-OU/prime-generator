5329e19b9e875f5f8ece8e4056c0fab4ee70a7da
JennyR-OU <jennyanneriley@live.co.uk>
2025-01-18 11:53:03 +0000

    Added build pipeline and initial project files

---
+++ src/main/java/uk/ac/open/primegenerator/Main.java
@@ -0,0 +1,13 @@
+package uk.ac.open.primegenerator;
+
+public class Main {
+
+    public int generatePrime() {
+        return 13;
+    }
+
+    public static void main(String[] args) {
+        Main primeGenerator = new Main();
+        System.out.println(primeGenerator.generatePrime());
+    }
+}
